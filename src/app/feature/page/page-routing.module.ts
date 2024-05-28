67d68a9983c3876ea8a6b079b492b0a095c8355d
Himanshu Patkar <Himanshupatkar274@gmail.com>
2024-05-28 12:10:50 +0530

    Create_components and add UI changes

--- src/app/feature/page/page-routing.module.ts
+++ src/app/feature/page/page-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageComponent } from './page/page.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
-    path:'', component:PageComponent
+    path:'', component:HomeComponent,
   },
+  { path:'about', component:AboutComponent,}
 ];
 
 @NgModule({
--- src/app/feature/page/page.module.ts
+++ src/app/feature/page/page.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PageRoutingModule } from './page-routing.module';
 import { PageComponent } from './page/page.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
-    PageComponent
+    PageComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
